81b290c994244bead875f468bfb9f7b8b0662ce3
Vinaya <vinayarajput0716@gmail.com>
2024-12-03 23:48:50 +0530

    added

--- modules/database.js
+++ modules/database.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 
 async function connectToDatabase() {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+        await mongoose.connect(process.env.MONGO_URI,{
+            ssl: true
         });
         console.log('MongoDB connected');
     } catch (err) {
