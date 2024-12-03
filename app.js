2450bc465b54ae835b8a673cae6e778f6fbdc38d
Vinaya <vinayarajput0716@gmail.com>
2024-12-03 22:29:03 +0530

    db

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/static', express.static(path.join(__dirname, 'static')));
+connectToDatabase();
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
